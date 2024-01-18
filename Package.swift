af2a66c49d9db714bb38d4e13e3e7e88ceeaca01
Sulaiman Ghori <sulaiman.ghori@outlook.com>
2024-01-18 14:54:55 -0800

    :zap: (Acceleration) Enable CORE_ML fallback on dev builds
    Via unsafe flag, see fast branch on parent repo for more details

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@ import PackageDescription
 var exclude: [String] = []
 
 #if os(Linux)
-// Linux doesn't support CoreML, and will attempt to import the coreml source directory
-exclude.append("coreml")
+    // Linux doesn't support CoreML, and will attempt to import the coreml source directory
+    exclude.append("coreml")
 #endif
 
 let package = Package(
@@ -14,18 +14,23 @@ let package = Package(
         .iOS(.v14),
         .macOS(.v12),
         .watchOS(.v4),
-        .tvOS(.v14)
+        .tvOS(.v14),
     ],
     products: [
-        .library(name: "SwiftWhisper", targets: ["SwiftWhisper"])
+        .library(name: "SwiftWhisper", targets: ["SwiftWhisper"]),
     ],
     dependencies: [
         .package(url: "https://github.com/ggerganov/whisper.cpp", .branchItem("master")),
     ],
     targets: [
-        .target(name: "SwiftWhisper", dependencies: [.product(name: "whisper", package: "whisper.cpp")]),
-        .testTarget(name: "WhisperTests", dependencies: [.target(name: "SwiftWhisper")], resources: [.copy("TestResources/")])
+        .target(name: "SwiftWhisper", dependencies: [.product(name: "whisper", package: "whisper.cpp")],
+                cSettings: [
+                    .define("GGML_USE_ACCELERATE", .when(platforms: [.macOS, .macCatalyst, .iOS])),
+                    .define("WHISPER_USE_COREML", .when(platforms: [.macOS, .macCatalyst, .iOS])),
+                    .define("WHISPER_COREML_ALLOW_FALLBACK", .when(platforms: [.macOS, .macCatalyst, .iOS])),
+                    .unsafeFlags(["-O3"]),
+                ]),
+        .testTarget(name: "WhisperTests", dependencies: [.target(name: "SwiftWhisper")], resources: [.copy("TestResources/")]),
     ],
     cxxLanguageStandard: CXXLanguageStandard.cxx11
 )
-
